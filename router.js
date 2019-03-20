5c4435027901f0ddb97ad9424108f3b5fb022d90
dislersd <dislersd@gmail.com>
2019-03-20 14:52:07 -0700

    created post and put routes for users

--- router.js
+++ router.js
@@ -30,4 +30,27 @@ router.get("/users/:id", (req, res) => {
     });
 });
 
+router.post("/users", async (req, res) => {
+  try {
+    const user = await userDb.insert(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: "error adding user"
+    });
+  }
+});
+
+router.put("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const update = await userDb.update(id, req.body);
+    res.status(200).json(update);
+  } catch (error) {
+    res.status(500).json({
+      message: "error updating"
+    });
+  }
+});
+
 module.exports = router;
