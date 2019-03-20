2f3b4fe9f364aa3b20afc38359bcfa0372ffb51f
dislersd <dislersd@gmail.com>
2019-03-20 13:58:44 -0700

    set up express server

---
+++ router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const userDb = require("./data/helpers/userDb");
+const postDb = require("./data/helpers/postDb");
+
+const router = express.Router();
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const server = express();
+
+const router = require('./router.js')
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`Hello`)
+});
+
+server.use(router)
+
+module.exports = server;
