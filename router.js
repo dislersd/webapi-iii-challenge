dd6905fbe27a12aad80d7f3b125150f211651377
dislersd <dislersd@gmail.com>
2019-03-20 14:22:29 -0700

    created get and getById routes for users

--- router.js
+++ router.js
@@ -5,4 +5,29 @@ const postDb = require("./data/helpers/postDb");
 
 const router = express.Router();
 
+router.get("/users", async (req, res) => {
+  try {
+    const users = await userDb.get();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: "error getting users"
+    });
+  }
+});
+
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .getById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "error getting user"
+      });
+    });
+});
+
 module.exports = router;
