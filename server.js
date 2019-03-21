c82b0a9038fa39d6b399e9fbfed4d7f8243a2b77
dislersd <dislersd@gmail.com>
2019-03-21 13:23:44 -0700

    set up environment variable

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express')
+const express = require("express");
 const server = express();
-const router = require('./router.js')
-
+const router = require("./router.js");
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.send(`Hello`)
+server.get("/", async (req, res) => {
+  const env = process.env.HELLO;
+  try {
+    res.status(200).send(env);
+  } catch (error) {
+    res.status(500).json({ message: "error loading page" });
+  }
 });
 
-server.use(router)
+server.use(router);
 
 module.exports = server;
